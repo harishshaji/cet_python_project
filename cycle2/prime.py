60ce97a7fd97911e3d377830a9e7cb20c1cde3e7
Harish shaji <93399652+harishshaji@users.noreply.github.com>
2021-12-31 12:31:40 +0530

    Update prime.py

--- cycle2/prime.py
+++ cycle2/prime.py
@@ -1,8 +1,8 @@
 a = int(input("enter a number"))
-flag = 0;
-n = a.len()
-for i in range(0,n)
-	if a % i == 0
+flag = 0
+
+for i in range(2,a):
+	if a % i == 0:
 		flag = 1
 if flag == 0:
     print(a,"is Prime")
